d2ba1fef1d5080aff02a6963dc11190719973b28
a123456683 <59089635+a123456683@users.noreply.github.com>
2022-05-30 20:27:34 +0800

    Delete src directory

--- src/main/java/com/mindlinker/listengitlab/Untis/CompareBranchUtils.java
+++
@@ -1,7 +0,0 @@
-package com.mindlinker.listengitlab.Untis;
-
-import java.io.IOException;
-
-public interface CompareBranchUtils {
-    boolean compareBranchIsDiff(String projectId, String gitCommitId) throws IOException;
-}
--- src/main/java/com/mindlinker/listengitlab/Untis/MergeRequestUtils.java
+++
@@ -1,7 +0,0 @@
-package com.mindlinker.listengitlab.Untis;
-
-import java.io.UnsupportedEncodingException;
-
-public interface MergeRequestUtils {
-    String createMergeRequest(String projectId, String gitCommitId, int assigneeId) throws UnsupportedEncodingException;
-}
